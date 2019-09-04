d999e57ac60f51f7a5080e778a9286b679dcbd69
nemosity <20733499+nemosity@users.noreply.github.com>
2019-09-04 17:51:34 +1200

    Publish to github pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44c737fd4cc1d71f7c3cb75785c62a07.js"
+  "/react-formulate-demo/precache-manifest.dfdeddefd433f928bdb2353f55ce65e5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-formulate-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
