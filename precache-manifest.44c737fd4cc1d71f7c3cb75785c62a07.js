c480be00735899d0f318538e903c2997fbea76db
nemosity <20733499+nemosity@users.noreply.github.com>
2019-09-04 17:31:02 +1200

    Publish to github pages

---
+++ precache-manifest.44c737fd4cc1d71f7c3cb75785c62a07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c00367600899b5d94851",
+    "url": "/static/js/main.f1792244.chunk.js"
+  },
+  {
+    "revision": "c4f2406e3d3d4019f9e0",
+    "url": "/static/js/2.f9b8f082.chunk.js"
+  },
+  {
+    "revision": "c00367600899b5d94851",
+    "url": "/static/css/main.50cb2c93.chunk.css"
+  },
+  {
+    "revision": "212c3ec7f274adbbf8f6a38a3105f32a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-formulate-demo/precache-manifest.b6cc4d2b7bf62fe2fae529a1fe129b54.js"
+  "/precache-manifest.44c737fd4cc1d71f7c3cb75785c62a07.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-formulate-demo/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
